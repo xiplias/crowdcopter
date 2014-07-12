3bb5a33b6d4af3b14dc9f997fbac67d3a132a14f
Anders Hansen <xiplias@gmail.com>
2014-07-12 16:35:48 +0200

    Adding frontend

---
+++ static/controller.js
@@ -0,0 +1,59 @@
+var json = require('through-json')
+var pumpify = require('pumpify')
+var websocket = require('websocket-stream')
+var _ = require('lodash')
+
+connectWebsocket();
+
+var s = {
+  "w": "forward",
+  "a": "left",
+  "d": "right",
+  "s": "back",
+  "up": "up",
+  "left": "rotate-left",
+  "right": "rotate-right",
+  "down": "down",
+  "t": "take-off",
+  "l": "land"
+}
+
+setInterval(function () {
+  if (stream) stream.write("ping");
+}, 1000);
+
+$.each(s, function(key, value) {
+  KeyboardJS.on(key, function() {
+    console.log(key, value)
+    stream.write(value);
+    $("#"+value).addClass("highlight");
+  }, function () {
+    $("#"+value).removeClass("highlight");
+  });
+
+  $("#"+value).click(function () {
+    console.log(key, value)
+    stream.write(value);
+  })
+});
+
+function connectWebsocket () {
+  var test =  pumpify.obj(json.stringify(), websocket('wss://drones2.localtunnel.me'), json.parse())
+
+  test.on('data', function (data) {
+    $("#current").html(data);
+    console.log("data", data);
+  });
+
+  test.on("error", function (e) {
+    setTimeout(function () {
+      connectWebsocket();
+    }, 1000);
+  });
+
+  test.on("open", function () {
+    console.log("online");
+  })
+
+  window.stream = test;
+}
